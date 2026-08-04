a4ecd80aace478a02d15f38e97bc3b1e2bfcfdab
bintangketjil <asterisk.burrow@gmail.com>
2026-08-05 06:41:29 +0700

    Add parse_variant() to 2fontface

---
+++ utils/filter/remove-p-anchor.lua
@@ -0,0 +1,14 @@
+local targets = {
+   "Image",
+   "Link"
+}
+
+function Para(element)
+   if #element.content == 1 then
+      for _, target in ipairs(targets) do
+	 if element.content[1].t == target then
+	    return element.content[1]
+	 end
+      end
+   end
+end
---
+++ utils/filter/unwrapper.lua
@@ -0,0 +1,15 @@
+local targets = {
+   "Image",
+   "Link",
+   "LineBreak"
+}
+
+function Para(element)
+   if #element.content == 1 then
+      for _, target in ipairs(targets) do
+	 if element.content[1].t == target then
+	    return element.content[1]
+	 end
+      end
+   end
+end
